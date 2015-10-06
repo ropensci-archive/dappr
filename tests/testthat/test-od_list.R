6ca7698ed9a567e71c0b6eca750a6fd34add329e
Scott Chamberlain <myrmecocystus@gmail.com>
2015-10-05 18:00:26 -0700

    added one test to get started

---
+++ tests/testthat/test-od_list.R
@@ -0,0 +1,21 @@
+context("od_list")
+
+test_that("basic usage works", {
+  skip_on_cran()
+
+  aa <- od_list("MERRA_MONTHLY")
+  bb <- od_list("MERRA_MONTHLY", "MATMNXLND.5.2.0")
+  cc <- od_list("MERRA_MONTHLY", "MSTMNXMLD.5.2.0", "2000")
+
+  expect_is(aa, "data.frame")
+  expect_is(bb, "data.frame")
+  expect_is(cc, "data.frame")
+  expect_is(aa$name, "character")
+
+  expect_more_than(NROW(bb), NROW(aa))
+  expect_more_than(NROW(cc), NROW(aa))
+})
+
+test_that("od_list fails well with no input", {
+  expect_error(od_list(), "Please pass in some values")
+})
